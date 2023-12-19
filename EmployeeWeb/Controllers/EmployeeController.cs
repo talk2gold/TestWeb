ec82bebde133c192044a87633831f42a8373ccec
Thangamani Eraniyan <talk2gold@live.com>
2023-12-18 23:15:23 -0800

    employee

---
+++ EmployeeWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,107 @@
+﻿using Dapper;
+using EmployeeWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace EmployeeWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IConfiguration _config;
+        private IDbConnection _db;
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+            _db = new OracleConnection(_config.GetConnectionString("default"));
+        }
+        public IActionResult EmployeeList() 
+        {
+            //List of Employees
+            //FirstName,LastName,Birth Date,DeptName,Manager Name, Join Date, Salary ( hidden : Empno, DeptId, MgrId)
+            string sql = @"           
+                            Select
+                              Emp.Employee_id EmpNo, Emp.First_Name FirstName, Emp.Last_Name LastName
+                            , Emp.Salary Salary, Emp.Hire_Date JoinDate
+                            , Dept.Department_Name DeptName
+                            , mgr.First_Name || ', ' || mgr.Last_Name MgrName
+                            From Employees emp
+                                 left join Departments Dept on Dept.Department_ID = Emp.Department_id
+                                 left join Employees mgr on mgr.Employee_id = emp.Manager_ID  
+                          ";
+
+            IEnumerable<EmployeeVM> emplist = _db.Query<EmployeeVM>(sql);
+            return View(emplist); 
+        }
+        public ActionResult Create()
+        {
+            EmployeeUpsertVM empUpsert = new();  //emptry emplyee structure
+
+            string sql;
+            //getting department
+            sql = "select DEPARTMENT_ID Id, DEPARTMENT_NAME DeptName, MANAGER_ID MgrNo from departments order by 2";
+            IEnumerable<Departement> deptList = _db.Query<Departement>(sql);
+            empUpsert.Depts = deptList.Select(a => new SelectListItem
+            {
+                Text = a.DeptName,
+                Value = a.Id.ToString(),
+            });
+
+
+
+            //get manager
+            sql = @"Select
+                          mgr.Employee_id MrgId, mgr.First_Name || ', ' || mgr.Last_Name MgrName
+                     From Employees mgr order by 2
+                   ";
+            IEnumerable<Manager> emplist = _db.Query<Manager>(sql);
+            empUpsert.Managers = emplist.Select(a => new SelectListItem
+            {
+                Text = a.MgrName,
+                Value = a.MrgId.ToString(),
+            });
+
+            return View(empUpsert);
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeUpsertVM empUpsert)
+        {
+            int empno = _db.Query<int>("select max(Employee_id) +1 empno from Employees");
+
+            string sql = "Insert Into Employee(employee_id) values("");
+                return View(empUpsert);
+        }
+
+        public ActionResult Edit(EmployeeUpsertVM empUpsert)
+        {
+
+
+            string sql;
+            //getting department
+            sql = "select DEPARTMENT_ID Id, DEPARTMENT_NAME DeptName, MANAGER_ID MgrNo from departments order by 2";
+            IEnumerable<Departement> deptList = _db.Query<Departement>(sql);
+            empUpsert.Depts = deptList.Select(a => new SelectListItem
+            {
+                Text = a.DeptName,
+                Value = a.Id.ToString(),
+                Selected= a.Id.Equals(empUpsert.DeptID)
+            });
+
+
+            //get manager
+            sql = @"Select
+                          mgr.Employee_id MrgId, mgr.First_Name || ', ' || mgr.Last_Name MgrName
+                     From Employees mgr order by 2
+                   ";
+            IEnumerable<Manager> emplist = _db.Query<Manager>(sql);
+            empUpsert.Managers = emplist.Select(a => new SelectListItem
+            {
+                Text = a.MgrName,
+                Value = a.MrgId.ToString(),
+            });
+
+            return View(empUpsert);
+        }
+    }
+}
---
+++ EmployeeWeb/Models/Employee.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EmployeeWeb.Models
+{
+    //currently we are not going to use this
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; } 
+        public DateTime JoinDate { get; set; } 
+        public decimal Salary {get; set; } 
+        public int MgrNo{ get; set; } 
+        public int DeptID {get; set; }
+        
+    }
+
+
+    // list employess
+    public class EmployeeVM
+    {
+        public int EmpNo { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal Salary { get; set; }
+        public string MgrName { get; set; } = string.Empty;
+        public string DeptName { get; set; } = string.Empty;
+    }
+
+    public class EmployeeUpsertVM   //Update /Insert
+    {
+        public int EmpNo { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public DateTime JoinDate { get; set; }
+        public decimal Salary { get; set; }
+        public int MgrNo { get; set; }
+        public int DeptID { get; set; }
+
+        public IEnumerable<SelectListItem> Depts { get; set; }
+        public IEnumerable<SelectListItem> Managers { get; set; }
+
+     
+    }
+
+    public class Departement
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; } = string.Empty;
+        public int MgrNo {get;set; }
+    }
+    public class Manager
+    {
+        public int MrgId { get; set; }
+        public string MgrName { get; set; } = string.Empty;
+    }
+
+}
